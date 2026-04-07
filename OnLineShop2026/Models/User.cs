b86c27b90785cd2aec7d85500f3bcfe959ba858b
Sarmat <Sarmat@DESKTOP-JKHAPIE>
2026-04-07 22:48:39 +0300

    переделал задание 1

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,26 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string AvatarPath { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+            AvatarPath = "/images/default-avatar.png";
+        }
+
+        public User(string firstName, string lastName, string email) : this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
