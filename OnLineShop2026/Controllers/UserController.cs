a7a7fd97dc2ed1804ec5d32a23f4b075b9ab98f2
Sarmat <Sarmat@DESKTOP-JKHAPIE>
2026-04-07 22:50:56 +0300

    переделал задание 2

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Profile()
+        {
+           
+            var user = new User("Сармат", "Макиев", "MAK@MAIL.ru");
+
+            return View(user);
+        }
+    }
+}
